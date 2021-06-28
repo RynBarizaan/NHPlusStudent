a7d407c6bf3b1231b9dc3225c2d3cbaf7eb0acf2
niklas.j2 <niklas.j2@schule.bremen.de>
2021-06-28 14:18:01 +0200

    Modellklasse Caregiver implementiert; Modellklasse Patient Java-Doc: "nurses" zu "caregivers" geändert

---
+++ src/main/java/model/Caregiver.java
@@ -0,0 +1,79 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Caregivers work in a NURSING home and are treating patients.
+ */
+public class Caregiver extends Person{
+    private long cid;
+    private String telnumber;
+    private boolean active;
+    private List<Treatment> allTreatments = new ArrayList<Treatment>();
+
+    /**
+     * constructs a caregiver from the given params.
+     * @param firstName
+     * @param surname
+     * @param telnumber
+     * @param active
+     */
+    public Caregiver(String firstName, String surname, String telnumber, Boolean active) {
+        super(firstName,surname);
+        this.telnumber = telnumber;
+        this.active = active;
+    }
+
+    /**
+     * constructs a caregiver from the given params.
+     * @param cid
+     * @param firstName
+     * @param surname
+     * @param telnumber
+     * @param active
+     */
+    public Caregiver(long cid, String firstName, String surname, String telnumber, Boolean active) {
+        super(firstName,surname);
+        this.cid = cid;
+        this.telnumber = telnumber;
+        this.active = active;
+    }
+
+    /**
+     *
+     * @return caregiver id
+     */
+    public long getCid() {
+        return cid;
+    }
+
+    /**
+     *
+     * @param telnumber new telephone number
+     */
+    public void setTelnumber(String telnumber) {
+        this.telnumber = telnumber;
+    }
+
+    /**
+     *
+     * @param active set new activity state
+     */
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    /**
+     *
+     * @return string-representation of the caregiver
+     */
+    public String toString() {
+        return "Caregiver" + "\nCID: " + this.cid +
+                "\nFirstname: " + this.getFirstName() +
+                "\nSurname: " + this.getSurname() +
+                "\nTelephone: " + this.telnumber +
+                "\nActive: " + this.active +
+                "\n";
+    }
+}
